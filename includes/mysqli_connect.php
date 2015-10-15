1d69fc9b4830e920803116ad93cd2e046c5e82d2
Andrew McCluckie <andy@amc21.co.uk>
2015-10-15 11:49:35 +0100

    Updated for local testing at work. See code comments.
    Projects.js output working fine.

--- includes/mysqli_connect.php
+++ includes/mysqli_connect.php
@@ -1,13 +1,1 @@
-<?php # Script 8.2 - mysqli_connect.php
-
-//Contains database access info
-//Establishes connection to MySQL & selects the database
-
-//Set access info as constants
-
-DEFINE ('DB_USER', 'test2');
-DEFINE ('DB_PASSWORD', 'been21');
-DEFINE ('DB_HOST', 'localhost');
-DEFINE ('DB_NAME', 'swaprojects');
-
-$dbc = mysqli_connect (DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) OR die ('Cold not connect to MySQL: ' . mysqli_connect_error() ); //@ = error suppression 
+<?php # Script 8.2 - mysqli_connect.php//Contains database access info//Establishes connection to MySQL & selects the database//Set access info as constantsDEFINE ('DB_USER', 'test2');DEFINE ('DB_PASSWORD', 'been21');DEFINE ('DB_HOST', 'localhost');DEFINE ('DB_NAME', 'swaprojects'); // or try 'projects'$dbc = mysqli_connect (DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) OR die ('Cold not connect to MySQL: ' . mysqli_connect_error() ); //@ = error suppression 
--- includes/projects-output.php
+++ includes/projects-output.php
@@ -1,47 +1,1 @@
-<?php
-
-	$root = strtolower( $_SERVER['DOCUMENT_ROOT'] );
-//	echo '<p>' . $root . '</p>';
-	
-	// Check if we are running on local or remote server and load correct DB_CONN file
-	if ( strpos($root, 'htdocs') || strpos($root, 'localhost') !== FALSE ) {
-		require_once('mysqli_connect.php');
-	} else {
-		require_once('mysqli_connect_swa.php');	
-	}
-
-	$q = "SELECT id, name, completion, keywords FROM swaprojects.projects WHERE 1";
-
-	$r = mysqli_query($dbc, $q); // Run the query.
-	$num_rows = mysqli_num_rows($r); // Count results returned - need this for trailing comma
-
-	// Open file, if doesn't exist create it
-	$file = fopen("projects.js", "w");
-	
-	// Set counter for trailing comma
-	$i = 0;
-	$jSON = "[ \n";
-	
-
-//	echo print_r($r);
-	while ( $row = mysqli_fetch_array($r, MYSQLI_ASSOC) ) {
-		$i++;
-		$jSON .= '{';
-		$jSON .= '"id": ' . $row['id'] . ',';
-		$jSON .= '"name": "' . $row['name'] . '",';
-		$jSON .= '"completion": "' . $row['completion'] . '",';
-		$jSON .= '"keywords": "' . $row['keywords'] . '",';
-		$jSON .= '"link": "/' . str_replace(' ', '-', $row['name']) . '/?proj-id=' . $row['id']  . '"';
-		$jSON .= '}';
-		
-		// If NOT the last row then ass a trailing comma
-		if ($i < $num_rows) {
-			$jSON .= ',';	
-		}
-	}
-	
-	$jSON .= '];';
-	
-	fwrite($file, $jSON);
-	fclose($file);
-?>
+<?php	$root = strtolower( $_SERVER['DOCUMENT_ROOT'] );//	echo '<p>' . $root . '</p>';		// Check if we are running on local or remote server and load correct DB_CONN file	if ( strpos($root, 'htdocs') || strpos($root, 'localhost') !== FALSE ) {		require_once('mysqli_connect.php');	} else {		require_once('mysqli_connect_swa.php');		}	$q = "SELECT id, name, completion, keywords FROM swaprojects.projects WHERE 1";	//$q = "SELECT id, name, completion, keywords FROM projects.projects WHERE 1";	$r = mysqli_query($dbc, $q); // Run the query.	$num_rows = mysqli_num_rows($r); // Count results returned - need this for trailing comma	// Open file, if doesn't exist create it	$file = fopen("projects.js", "w");		// Set counter for trailing comma	$i = 0;	$jSON = "[ \n";	//	echo print_r($r);	while ( $row = mysqli_fetch_array($r, MYSQLI_ASSOC) ) {		$i++;		$jSON .= '{';		$jSON .= '"id": ' . $row['id'] . ',';		$jSON .= '"name": "' . $row['name'] . '",';		$jSON .= '"completion": "' . $row['completion'] . '",';		$jSON .= '"keywords": "' . $row['keywords'] . '",';		$jSON .= '"link": "/' . str_replace(' ', '-', $row['name']) . '/?proj-id=' . $row['id']  . '"';		$jSON .= '}';				// If NOT the last row then ass a trailing comma		if ($i < $num_rows) {			$jSON .= ',';			}	}		$jSON .= '];';		fwrite($file, $jSON);	fclose($file);?>
